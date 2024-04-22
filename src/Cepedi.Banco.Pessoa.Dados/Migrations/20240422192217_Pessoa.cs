39609f66ff091ee8c6e4ed290add707713690bd3
albertolunia <albertolunia@gmail.com>
2024-04-22 16:29:55 -0300

    Atividade 22/04

---
+++ src/Cepedi.Banco.Pessoa.Dados/Migrations/20240422192217_Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cepedi.Banco.Pessoa.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pessoa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Pessoa",
+                columns: new[] { "Id", "Cpf", "DataNascimento", "Email", "EstadoCivil", "Genero", "Nacionalidade", "Nome", "Telefone" },
+                values: new object[] { 1, "12345678901", new DateTimeOffset(new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -2, 0, 0, 0)), "teste", "Solteiro", "M", "Brasileiro", "João da Silva", "11999999999" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Pessoa",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ teste.session.sql
@@ -0,0 +1,22 @@
+INSERT INTO Pessoa (
+    Id,
+    Nome,
+    Email,
+    DataNascimento,
+    Cpf,
+    Telefone,
+    Genero,
+    EstadoCivil,
+    Nacionalidade
+  )
+VALUES (
+    'Id:1',
+    'Nome:TEXT',
+    'Email:TEXT',
+    'DataNascimento:TEXT',
+    'Cpf:TEXT',
+    'Telefone:TEXT',
+    'Genero:TEXT',
+    'EstadoCivil:TEXT',
+    'Nacionalidade:TEXT'
+  );
